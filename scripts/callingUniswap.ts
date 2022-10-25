d478065b9a98c12b0d30713d0925ddb96445e425
Oleksii Bereza <95891690+bircher044@users.noreply.github.com>
2022-10-25 18:19:41 +0300

    added ETH swapping feature

--- scripts/callingUniswap.ts
+++ scripts/callingUniswap.ts
@@ -1,45 +1,68 @@
 import {ethers} from "hardhat";
-import { Contract, BigNumber, Wallet, ContractFactory, providers, Signer } from "ethers";
+import {BigNumber } from "ethers";
 import {UniswapV2ERC20__factory} from "../typechain-types";
 import {UniswapV2Router02__factory} from "../typechain-types";
 import * as dotenv from 'dotenv';
 
 const uniswapV2Router02ContractAddress = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"; //GOERLI
-const uniswapV2Factory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+
+//
+const tokenAmountToSwap : BigNumber = ethers.utils.parseEther("0.01")
+let deadline = 1000;
+const firstTokenAddressToSwap = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB"; //LINK
+const secondTokenAddressToSwap = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+const WETHAddress = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6";
+//
 
 const signer = new ethers.Wallet((String(process.env.MY_WALLET_PRIVATE_KEY)), ethers.provider);
-
-//
-const tokenAmountToSwap : BigNumber = BigNumber.from(10 ** 5);
-const slippageTollerance = 50; // 1 = 0.01%
-const deadline = 1000;
-const firstTokenAddressToSwap = "0x326C977E6efc84E512bB9C30f76E30c160eD06FB"; //WETH
-const secondTokenAddressToSwap = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"; //LINK
-//
-
 const ERC20 = UniswapV2ERC20__factory.connect(firstTokenAddressToSwap, signer);
 const router02 = UniswapV2Router02__factory.connect(uniswapV2Router02ContractAddress, signer);
 
-const swap = async(amount : BigNumber, path : string[], ) => {
-    let token0 : string;
-    let token1 : string;
-    [token0, token1] = path[0] < path[1] ? (path[0], path[1]) : (path[1], path[0]);
+const swap = async(amount : BigNumber, path : string[], deadline ) => {
 
-    if(token0 == ethers.constants.AddressZero){
-        
+    if(path[0] == path[1]){
+        console.log("Error. Exact token addreses")
+    }
+
+    else if(path[0] == ethers.constants.AddressZero){
+
+        path[0] = WETHAddress;
+
+        console.log("Swapping ETH for Token");
+        const txReceipt = await (await router02.swapExactETHForTokens(0, path, signer.address, deadline, {value: amount})).wait();
+        txReceipt && txReceipt.blockNumber ? console.log("Transaction signed") : console.log("Transaction failed");
+    }
+
+    else if(path[1] == ethers.constants.AddressZero){
+
+        path[1] = WETHAddress;
+
+        console.log("Approving token spend");
+        const txReceipt1 = await (await ERC20.approve(router02.address, amount)).wait();
+        txReceipt1 && txReceipt1.blockNumber ? console.log("Transaction signed") : console.log("Transaction failed");
+
+        console.log("Swapping Token for ETH");
+        const txReceipt2 = await (await router02.swapExactTokensForETH(amount, 0, path, signer.address, deadline)).wait();
+        txReceipt2 && txReceipt2.blockNumber ? console.log("Transaction signed") : console.log("Transaction failed");
     }
 
     else{
-        await (await ERC20.approve(router02.address, amount)).wait();
-        console.log(await ERC20.allowance(signer.address, router02.address, ));
-        
-        await (await router02.swapExactTokensForTokens(amount, 0, path, signer.address, (await ethers.provider.getBlock("latest")).timestamp + deadline)).wait();
+
+        console.log("Approving token spend");
+        const txReceipt1 = await (await ERC20.approve(router02.address, amount)).wait();
+        txReceipt1 && txReceipt1.blockNumber ? console.log("Transaction signed") : console.log("Transaction failed");
+
+        console.log("Swapping Token for Token");
+        const txReceipt2 = await (await (router02.swapExactTokensForTokens(amount, 0, path, signer.address, deadline))).wait();
+        txReceipt2 && txReceipt2.blockNumber ? console.log("Transaction signed") : console.log("Transaction failed");
     }
 
 }
+
 const main = async () => {
 
-    await swap(tokenAmountToSwap, [firstTokenAddressToSwap, secondTokenAddressToSwap]);
+    deadline = ((await ethers.provider.getBlock("latest")).timestamp + deadline);
+    await swap(tokenAmountToSwap, [firstTokenAddressToSwap, secondTokenAddressToSwap], deadline);
 
 };
 
